42a440d5ddecfcba8c926f76e0f32663a9caab6c
Iden Amoako <120428406+iden-a@users.noreply.github.com>
2024-03-07 01:50:06 -0500

    Development (#3)
    * setting up the frontend/backend
    
    * updating the frontend
    
    * updated the forms and data component

--- demo-ui/src/components/Form/Form.jsx
+++ demo-ui/src/components/Form/Form.jsx
@@ -1,11 +1,108 @@
-import Navbar from "../Navbar/Navbar.jsx"
-// import { useState } from "react";
+import Navbar from "../Navbar/Navbar.jsx";
+import "../Form/Form.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Form() {
+  const navigateTo = useNavigate();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    collegeMajor: "",
+    funFact: "",
+  });
 
-    return(
-        <>
-        <Navbar/>
-        </>
-    )
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const clearForm = () => {
+    setFormData({
+        firstName: "",
+        lastName: "",
+        college: "",
+        major: "",
+        funFact: "",
+    })
+  }
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission here, e.g., send data to backend or display it
+    console.log(formData);
+    clearForm();
+    navigateTo("/data")
+  };
+
+  return (
+    <>
+      <Navbar />
+      <h2 className="heading">Enter Details</h2>
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <div className="first-last-container">
+            <div>
+              <p>First Name</p>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <p>Last Name</p>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="college-university">
+              <p>College/University</p>
+              <input
+                type="text"
+                id="collegeMajor"
+                name="collegeMajor"
+                value={formData.college}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="college-major ">
+              <p>Major</p>
+              <input
+                type="text"
+                id="collegeUniversity"
+                name="collegeUniversity"
+                value={formData.major}
+                onChange={handleChange}
+              />
+            </div>
+
+          <div className="fun-fact">
+            <p>Fun Fact</p>
+            <textarea
+              id="funFact"
+              name="funFact"
+              value={formData.funFact}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="form-btn">Submit</button>
+        </form>
+      </div>
+    </>
+  );
 }
--- demo-ui/src/components/Navbar/Navbar.jsx
+++ demo-ui/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,27 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Navbar/Navbar.css'
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <Link to="/">Back</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact">Option</Link>
-          </li>
-        </ul>
-      </nav>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Demo!</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="links collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/form">Form</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/data">Data</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     </>
   );
 }
